76dc29298d3d90c4e09d7b5c6d8865b24fc64650
sunbohit <a824834@163.com>
2017-06-13 22:20:29 +0800

    NLP_concat

--- NLP/NLP_baisc.py
+++ NLP/NLP_baisc.py
@@ -22,3 +22,14 @@ print(T)
 
 x = torch.randn((3, 4, 5))
 print(x)
+
+x_1 = torch.randn(2, 5)
+y_1 = torch.randn(3, 5)
+z_1 = torch.cat([x_1, y_1])
+print(z_1)
+
+# Concatenate columns:
+x_2 = torch.randn(2, 3)
+y_2 = torch.randn(2, 5)
+z_2 = torch.cat([x_2, y_2], 1) # second arg specifies which axis to concat along
+print(z_2)

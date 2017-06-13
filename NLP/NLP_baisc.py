b088443c04535aa03c0b9712e582613efa16bd56
sunbohit <a824834@163.com>
2017-06-13 22:25:52 +0800

    NLP_concat

--- NLP/NLP_baisc.py
+++ NLP/NLP_baisc.py
@@ -14,7 +14,6 @@ M_data = [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]
 M = torch.Tensor(M_data)
 print(M)
 
-# Create a 3D tensor of size 2x2x2.
 T_data = [[[1.,2.], [3.,4.]],
           [[5.,6.], [7.,8.]]]
 T = torch.Tensor(T_data)
@@ -28,8 +27,12 @@ y_1 = torch.randn(3, 5)
 z_1 = torch.cat([x_1, y_1])
 print(z_1)
 
-# Concatenate columns:
 x_2 = torch.randn(2, 3)
 y_2 = torch.randn(2, 5)
-z_2 = torch.cat([x_2, y_2], 1) # second arg specifies which axis to concat along
+z_2 = torch.cat([x_2, y_2], 1) 
 print(z_2)
+
+x = torch.randn(2, 3, 4)
+print(x)
+print(x.view(2, 12))
+print(x.view(2, -1))

8ef2e70e3cb3559c0f41b7b2a7e84d2c2ac3bddc
sunbohit <a824834@163.com>
2017-06-28 20:40:54 +0800

    basic pretrain save

--- Basic/pretrainedModel.py
+++ Basic/pretrainedModel.py
@@ -17,3 +17,9 @@ resnet.fc = nn.Linear(resnet.fc.in_features, 100)
 images = Variable(torch.randn(10, 3, 256, 256))
 outputs = resnet(images)
 print(outputs.size())
+
+torch.save(resnet, 'model.pkl')
+model = torch.load('model.pkl')
+
+torch.save(resnet.state_dict(), 'params.pkl')
+resnet.load_state_dict(torch.load('params.pkl'))

6b89436760ce25659468947abc71c16eeb0f1a32
sunbohit <a824834@163.com>
2017-06-28 20:38:52 +0800

    basic pretrain

---
+++ Basic/pretrainedModel.py
@@ -0,0 +1,19 @@
+import torch 
+import torchvision
+import torch.nn as nn
+import numpy as np
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+from torch.autograd import Variable
+
+resnet = torchvision.models.resnet18(pretrained=True)
+
+for param in resnet.parameters():
+	param.requires_grad = False
+
+resnet.fc = nn.Linear(resnet.fc.in_features, 100)
+
+images = Variable(torch.randn(10, 3, 256, 256))
+outputs = resnet(images)
+print(outputs.size())

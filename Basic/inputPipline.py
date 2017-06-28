1cce320b5f8e9df1cb967ff877534585d2fdd3c3
sunbohit <a824834@163.com>
2017-06-28 20:25:47 +0800

    basic loader2

--- Basic/inputPipline.py
+++ Basic/inputPipline.py
@@ -22,3 +22,22 @@ train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            shuffle=True,
                                            num_workers=2)
 
+data_iter = iter(train_loader)
+images, labels = data_iter.next()
+
+for images, labels in train_loader:
+	pass
+
+class CustomDataset(data.Dataset):
+	def __init__(self):
+		pass
+	def __getitem__(self, index):
+		pass
+	def __len__(self):
+		return 0 
+
+custom_dataset = CustomDataset()
+train_loader = torch.utils.data.DataLoader(dataset=custom_dataset,
+                                           batch_size=100, 
+                                           shuffle=True,
+                                           num_workers=2)

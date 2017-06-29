e9186f01c11bf6e06badc9ed675d67a697841ed5
sunbohit <a824834@163.com>
2017-06-29 18:42:39 +0800

    NLP basic

--- NLP/NLP_baisc.py
+++ NLP/NLP_baisc.py
@@ -57,3 +57,12 @@ print(var_z.creator)
 var_z_data = var_z.data
 new_var_z = autograd.Variable( var_z_data )
 print(new_var_z.creator)#None
+
+x = autograd.Variable( torch.Tensor([1., 2., 3]), requires_grad=True )
+print(x.data)
+
+y = autograd.Variable( torch.Tensor([4., 5., 6]), requires_grad=True )
+z = x + y
+print(z.data)
+
+print(z.creator)
